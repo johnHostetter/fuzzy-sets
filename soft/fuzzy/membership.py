73c5b079ad653dbcde4f39656ab032a1de9daf55
John Wesley Hostetter <jwhostet@ncsu.edu>
2022-10-31 20:04:36 -0400

    Add FTARM code and example

--- soft/fuzzy/membership.py
+++ soft/fuzzy/membership.py
@@ -3,3 +3,32 @@ import numpy as np
 
 def gaussian(x, center, sigma):
     return np.exp(-1.0 * (np.power(x - center, 2) / np.power(sigma, 2)))
+
+
+def triangular(x, params):
+    """
+    Triangular membership function that receives an 'x' value, a
+    nd uses the 'params' to determine a degree of membership for 'x'.
+
+    https://www.mathworks.com/help/fuzzy/trimf.html
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION.
+    params : TYPE
+        DESCRIPTION.
+    Returns
+    -------
+    mu : TYPE
+        DESCRIPTION.
+    """
+    if x <= params['a'] or params['c'] <= x:
+        mu = 0.0
+    elif params['a'] <= x and x <= params['b']:
+        mu = (x - params['a']) / (params['b'] - params['a'])
+    elif params['b'] <= x and x <= params['c']:
+        mu = (params['c'] - x) / (params['c'] - params['b'])
+    else:  # this is a catch-all, and should never be reached, but it is included nonetheless for exception handling
+        return None
+
+    return mu
