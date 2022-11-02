0f81de32bc7a81ec5c0949267848cae4a7343017
John Wesley Hostetter <jwhostet@ncsu.edu>
2022-11-02 02:27:47 -0400

    Remove unused code, as well as add Rule class and Learning Rate scheduler

--- soft/fuzzy/membership.py
+++ soft/fuzzy/membership.py
@@ -8,33 +8,17 @@ def gaussian(x, center, sigma):
 def triangular(x, center, width):
     """
     Triangular membership function that receives an 'x' value, a
-    nd uses the 'params' to determine a degree of membership for 'x'.
+    nd uses the 'center' and 'width' to determine a degree of membership for 'x'.
 
     https://www.mathworks.com/help/fuzzy/trimf.html
-    Parameters
-    ----------
-    x : TYPE
-        DESCRIPTION.
-    params : TYPE
-        DESCRIPTION.
-    Returns
-    -------
-    mu : TYPE
-        DESCRIPTION.
+    Args:
+        x:
+        center:
+        width:
+
+    Returns:
+
     """
     values = 1.0 - (1.0 / width) * np.abs(x - center)
     values[(values < 0)] = 0
     return values
-
-    params = {'a': center - width, 'b': center, 'c': center + width}
-
-    if x <= params['a'] or params['c'] <= x:
-        mu = 0.0
-    elif params['a'] <= x and x <= params['b']:
-        mu = (x - params['a']) / (params['b'] - params['a'])
-    elif params['b'] <= x <= params['c']:
-        mu = (params['c'] - x) / (params['c'] - params['b'])
-    else:  # this is a catch-all, and should never be reached, but it is included nonetheless for exception handling
-        return None
-
-    return mu
