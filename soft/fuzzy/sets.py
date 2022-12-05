6a011b545773b1d55755d06af1911686ec10a3ec
John Wesley Hostetter <jwhostet@ncsu.edu>
2022-12-05 15:57:45 -0500

    Remove debug print statements

--- soft/fuzzy/sets.py
+++ soft/fuzzy/sets.py
@@ -212,11 +212,8 @@ class Gaussian(Base):
         """
         # https://stackoverflow.com/questions/65022269/how-to-use-a-learnable-parameter-in-pytorch-constrained-between-0-and-1
 
-        try:
-            log_results = torch.exp(
-                -1.0 * (torch.pow(x.unsqueeze(dim=-1) - self.centers, 2) / torch.pow(torch.exp(self._log_widths), 2)))
-        except Exception:
-            print('hang on')
+        log_results = torch.exp(
+            -1.0 * (torch.pow(x.unsqueeze(dim=-1) - self.centers, 2) / torch.pow(torch.exp(self._log_widths), 2)))
         # no_log_results = torch.exp(
         #     -1.0 * (torch.pow(x.unsqueeze(dim=-1) - self.centers, 2) / torch.pow(self.widths, 2)))
         return log_results
