6ebd2f8b1c7cd75518d09ac25a888a19553ad3dd
John Wesley Hostetter <jhostetter16@gmail.com>
2023-12-05 10:22:56 -0500

    Begin refactoring fuzzy sets and FLC granulation layers

--- soft/fuzzy/sets/continuous.py
+++ soft/fuzzy/sets/continuous.py
@@ -1,7 +1,7 @@
 """
 Implements the continuous fuzzy sets, using PyTorch.
 """
-from typing import List
+from typing import List, NoReturn, Union, Tuple
 
 import torch
 import torchquad
@@ -45,23 +45,140 @@ class LogisticCurve(torch.nn.Module):
         )
 
 
+class GroupedFuzzySets(torch.nn.Module):
+    """
+    A generic and abstract torch.nn.Module class that contains a torch.nn.ModuleList
+    of ContinuousFuzzySet objects. The expectation here is that each ContinuousFuzzySet
+    may define fuzzy sets of different conventions, such as Gaussian, Triangular, Trapezoidal, etc.
+    Then, subsequent inference engines can handle these heterogeneously defined fuzzy sets
+    with no difficulty. Further, this class was specifically designed to incorporate dynamic
+    addition of new fuzzy sets in the construction of neuro-fuzzy networks via network morphism.
+
+    However, this class does *not* carry out any functionality that is necessarily tied to fuzzy
+    sets, it is simply named so as this was its intended purpose - grouping fuzzy sets. In other
+    words, the same "trick" of using a torch.nn.ModuleList of torch.nn.Module objects applies to
+    any kind of torch.nn.Module object.
+    """
+
+    def __init__(self, modules=None, expandable=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if modules is None:
+            modules = []
+        self.modules_list = torch.nn.ModuleList(modules)
+        self.expandable = expandable
+        self.epsilon = 0.5  # epsilon-completeness
+
+    def get_mask(self) -> Union[torch.Tensor, NoReturn]:
+        if len(self.modules_list) > 0:
+            module_masks: List[
+                torch.Tensor
+            ] = []  # the secondary response denoting module filter
+            for module in self.modules_list:
+                module_masks.append(module.get_mask().float())
+            return torch.cat(module_masks, dim=-1)
+        else:
+            raise ValueError("The torch.nn.ModuleList of GroupedFuzzySets is empty.")
+
+    def calculate_module_responses(
+        self, observations
+    ) -> Union[Tuple[torch.Tensor, torch.Tensor], NoReturn]:
+        if len(self.modules_list) > 0:
+            # modules' responses are membership degrees when modules are ContinuousFuzzySet
+            module_responses: List[
+                torch.Tensor
+            ] = []  # the primary response from the module
+            module_masks: List[
+                torch.Tensor
+            ] = []  # the secondary response denoting module filter
+            for module in self.modules_list:
+                module_response, module_mask = module(observations)
+                module_responses.append(module_response)
+                module_masks.append(module_mask.float())
+            return torch.cat(module_responses, dim=-1), torch.cat(module_masks, dim=-1)
+        else:
+            raise ValueError("The torch.nn.ModuleList of GroupedFuzzySets is empty.")
+
+    def forward(self, observations):
+        observations = convert_to_tensor(observations)
+        module_responses, module_masks = self.calculate_module_responses(observations)
+
+        # if mu.grad_fn is None and self.expandable:
+        #     print("grad_fn of mu is None, but might be in target net tho")
+
+        if self.expandable:
+            # find where the new centers should be added, if any
+            new_centers = (
+                observations * (module_responses.max(dim=-1).values <= self.epsilon)
+            ) + ((module_responses.max(dim=-1).values > self.epsilon) / 0).nan_to_num(
+                0.0, posinf=torch.nan
+            )
+            # print(mu.max(dim=-1).values, observations)
+            nc: List[torch.Tensor] = [
+                torch.tensor(
+                    list(
+                        set(
+                            new_centers[:, var_idx][
+                                ~new_centers[:, var_idx].isnan()
+                            ].tolist()
+                        )
+                    )
+                )
+                for var_idx in range(new_centers.shape[-1])
+            ]
+
+            # doing some trick w. dynamically expandable networks
+            sets: List[ContinuousFuzzySet] = []
+            empty_sets: List[int] = []
+            for idx, n in enumerate(nc):
+                if len(n) == 0:
+                    empty_sets.append(idx)
+                sets.append(
+                    Gaussian(
+                        in_features=len(n),
+                        centers=n,
+                        widths=torch.randn_like(n).abs(),
+                    )
+                )
+            if len(sets) > 0 and len(empty_sets) < len(sets):
+                # take the fuzzy sets, and make a ContinuousFuzzySet efficient object
+                g = ContinuousFuzzySet.stack(sets)
+                new_idx = len(self.modules_list)
+                print(
+                    f"adding {g.centers.shape}; num of modules already: {len(self.modules_list)}"
+                )
+                print(f"to dimensions: {set(range(len(sets))) - set(empty_sets)}")
+                self.modules_list.add_module(str(new_idx), g)
+                module_responses, module_masks = self.calculate_module_responses(
+                    observations
+                )
+
+        return module_responses, module_masks
+
+
 class ContinuousFuzzySet(torch.nn.Module):
     """
     A generic and abstract torch.nn.Module class that implements continuous fuzzy sets.
+
+    This is the most important Python class regarding fuzzy sets within this Soft Computing library.
+
+    Defined here are most of the common methods made available to all fuzzy sets. Fuzzy sets that
+    will later be used in other features such as neuro-fuzzy networks are expected to abide by the
+    conventions outlined within. For example, parameters 'centers' and 'widths' are often expected,
+    but inference engines (should) only rely on the fuzzy set membership degrees.
+
+    However, for convenience, some aspects of the SelfOrganize code may search for vertices that
+    have attributes of type 'ContinuousFuzzySet'. Thus, if it is pertinent that a vertex within
+    the KnowledgeBase is recognized as a fuzzy set, it is very likely one might be interested in
+    inheriting or extending from ContinuousFuzzySet.
     """
 
     def __init__(self, in_features, centers=None, widths=None, labels=None):
         super().__init__()
-        self.in_features, self._log_widths, self.labels = None, None, None
+        self.centers, self.widths, self.labels = None, None, None
+        self.in_features, self._log_widths, self.mask = None, None, None
         self.constructor(
             in_features=in_features, centers=centers, widths=widths, labels=labels
         )
-        self.modules_list = torch.nn.ModuleList([])
-        self.expandable = False
-
-    @staticmethod
-    def broadcast_modification(fuzzy_set: "ContinuousFuzzySet", *args, **kwargs):
-        pass
 
     def constructor(self, in_features, centers=None, widths=None, labels=None):
         self.in_features = in_features
@@ -270,7 +387,8 @@ class ContinuousFuzzySet(torch.nn.Module):
 
         return variables
 
-    def count_granule_terms(self, granules: List["ContinuousFuzzySet"]) -> np.ndarray:
+    @staticmethod
+    def count_granule_terms(granules: List["ContinuousFuzzySet"]) -> np.ndarray:
         """
         Count the number of granules that occur in each dimension.
 
@@ -287,11 +405,9 @@ class ContinuousFuzzySet(torch.nn.Module):
             ]
         ).astype("int32")
 
-    def stack_granules(
-        self,
+    @staticmethod
+    def stack(
         granules: List["ContinuousFuzzySet"],
-        in_place=False,
-        broadcast_set_modified=None,
     ) -> "ContinuousFuzzySet":
         """
         Create a condensed and stacked representation of the given granules.
@@ -303,7 +419,7 @@ class ContinuousFuzzySet(torch.nn.Module):
             A ContinuousFuzzySet object.
         """
         if list(granules)[0].training:
-            missing_center, missing_width = 0, -1
+            missing_center, missing_width = 0.0, -1.0
         else:
             missing_center = missing_width = torch.nan
 
@@ -313,7 +429,7 @@ class ContinuousFuzzySet(torch.nn.Module):
                     params.centers,
                     pad=(
                         0,
-                        self.count_granule_terms(granules).max()
+                        ContinuousFuzzySet.count_granule_terms(granules).max()
                         - params.centers.shape[0],
                     ),
                     mode="constant",
@@ -321,7 +437,7 @@ class ContinuousFuzzySet(torch.nn.Module):
                 )
                 if params.centers.dim() > 0
                 else torch.tensor(missing_center).repeat(
-                    self.count_granule_terms(granules).max()
+                    ContinuousFuzzySet.count_granule_terms(granules).max()
                 )
                 for params in granules
             ]
@@ -332,7 +448,7 @@ class ContinuousFuzzySet(torch.nn.Module):
                     params.widths,
                     pad=(
                         0,
-                        self.count_granule_terms(granules).max()
+                        ContinuousFuzzySet.count_granule_terms(granules).max()
                         - params.widths.shape[0],
                     ),
                     mode="constant",
@@ -340,31 +456,19 @@ class ContinuousFuzzySet(torch.nn.Module):
                 )
                 if params.centers.dim() > 0
                 else torch.tensor(missing_center).repeat(
-                    self.count_granule_terms(granules).max()
+                    ContinuousFuzzySet.count_granule_terms(granules).max()
                 )
                 for params in granules
             ]
         )
 
-        if in_place:
-            # directly modify *this* ContinuousFuzzySet object
-            with torch.no_grad():
-                print("Expanding the number of fuzzy sets!!!")
-                self.constructor(
-                    in_features=len(granules),
-                    centers=centers.cpu().detach().tolist(),
-                    widths=widths.cpu().detach().tolist(),
-                )
-                print(self.centers.shape)
-            self.broadcast_modification(self)
-        else:
-            # prepare a condensed and stacked representation of the granules
-            mf_type = type(granules[0])
-            return mf_type(
-                in_features=len(granules),
-                centers=centers.cpu().detach().tolist(),
-                widths=widths.cpu().detach().tolist(),
-            )
+        # prepare a condensed and stacked representation of the granules
+        mf_type = type(granules[0])
+        return mf_type(
+            in_features=len(granules),
+            centers=centers.cpu().detach().tolist(),
+            widths=widths.cpu().detach().tolist(),
+        )
 
     def forward(self, observations):
         """
@@ -381,7 +485,8 @@ class ContinuousFuzzySet(torch.nn.Module):
             None
         """
         raise NotImplementedError(
-            "The Base Fuzzy Set has no membership function defined."
+            "The ContinuousFuzzySet has no defined membership function. Please create a class and "
+            "inherit from ContinuousFuzzySet, or use a predefined class, such as Gaussian."
         )
 
 
@@ -413,8 +518,12 @@ class Gaussian(ContinuousFuzzySet):
         """
         self.widths = sigmas
 
-    def calculate_membership(self, observations):
-        mu = self.mask.to(observations.device) * (
+    def get_mask(self) -> torch.Tensor:
+        return (self.widths == -1.0).float()
+        return (torch.isclose(self.widths, torch.zeros(1))).float()
+
+    def calculate_membership(self, observations: torch.Tensor) -> torch.Tensor:
+        return (1 - self.get_mask()).to(observations.device) * (
             1
             / (
                 torch.pow(
@@ -429,26 +538,6 @@ class Gaussian(ContinuousFuzzySet):
             )
         )
 
-        # if mu.grad_fn is None:
-        #     print("memberships' autograd are not being tracked")
-
-        if len(self.modules_list) > 0:
-            returned_mu = mu
-            for module in self.modules_list:
-                other_mu, _ = module(observations)
-                returned_mu = torch.cat([returned_mu, other_mu], dim=-1)
-
-            return returned_mu
-        return mu
-
-    def get_mask(self):
-        mask = (self.widths == -1.0).float()
-        for module in self.modules_list:
-            mask = torch.cat(
-                [mask, (module.widths == -1).float().to(mask.device)], dim=-1
-            )
-        return mask
-
     def forward(self, observations):
         """
         Forward pass of the function. Applies the function to the input elementwise.
@@ -462,102 +551,7 @@ class Gaussian(ContinuousFuzzySet):
             The membership degrees of the observations for the Gaussian fuzzy set.
         """
         observations = convert_to_tensor(observations)
-
-        # print(self.widths)
-        # try:
-        #     for centers, widths in zip(self.centers, self.widths):
-        #         try:
-        #             if centers.isnan().all() or widths.isnan().all():
-        #                 print("its broken")
-        #                 # state_dictionary = self.state_dict()
-        #                 # state_dictionary["centers"] = self.previous_centers
-        #                 # state_dictionary["widths"] = self.previous_widths
-        #                 # self.load_state_dict(state_dictionary)
-        #                 # self.log_widths()
-        #                 # observations = self.previous_observations
-        #                 # quit()
-        #         except IndexError:
-        #             pass
-        # except TypeError:
-        #     pass
-        # self.previous_centers = self.centers.detach().clone()
-        # self.previous_widths = self.widths.detach().clone()
-        # self.previous_observations = observations.detach().clone()
-        # print(torch.exp(self._log_widths))
-        # m = torch.nn.Sigmoid()
-        # if self.widths.ndim == 2:
-        #     # calc_widths = torch.exp(self._log_widths).nan_to_num(
-        #     #     self.widths.max().item()
-        #     # ).min(-1).values.unsqueeze(-1) * m(torch.exp(self._log_widths))
-        #     calc_widths = m(torch.exp(self._log_widths))
-        # else:
-        #     calc_widths = torch.exp(self._log_widths)
-        mu = self.calculate_membership(observations)
-
-        # if mu.grad_fn is None and self.expandable:
-        #     print("grad_fn of mu is None, but might be in target net tho")
-
-        if self.centers.ndim == 2 and self.expandable:
-            epsilon = 0.5
-            new_centers = (observations * (mu.max(dim=-1).values <= epsilon)) + (
-                (mu.max(dim=-1).values > epsilon) / 0
-            ).nan_to_num(0.0, posinf=torch.nan)
-            # print(mu.max(dim=-1).values, observations)
-            nc: List[torch.Tensor] = [
-                torch.tensor(
-                    list(
-                        set(
-                            new_centers[:, var_idx][
-                                ~new_centers[:, var_idx].isnan()
-                            ].tolist()
-                        )
-                    ),
-                    dtype=self.centers.dtype,
-                )
-                for var_idx in range(new_centers.shape[-1])
-            ]
-
-            # editing in-place (doesn't work with arbitrary optimizers
-            # sets: List[ContinuousFuzzySet] = self.split_by_variables()
-            # for n, s in zip(nc, sets):
-            #     s.extend(
-            #         centers=n,
-            #         widths=torch.randn_like(n),
-            #     )
-            #
-            # self.stack_granules(sets, in_place=True)
-
-            # doing some trick w. dynamically expandable networks
-            sets: List[ContinuousFuzzySet] = []
-            empty_sets: List[int] = []
-            for idx, n in enumerate(nc):
-                if len(n) == 0:
-                    empty_sets.append(idx)
-                sets.append(
-                    Gaussian(
-                        in_features=len(n),
-                        centers=n,
-                        widths=torch.randn_like(n).abs(),
-                    )
-                )
-            if len(sets) > 0 and len(empty_sets) < len(sets):
-                g = self.stack_granules(sets)
-                g.expandable = False  # stop recursive calls
-                _, _ = g(observations)
-                new_idx = len(self.modules_list)
-                print(
-                    f"adding {g.centers.shape}; num of layers: {len(self.modules_list)}"
-                )
-                print(f"to dimensions: {set(range(len(sets))) - set(empty_sets)}")
-                self.modules_list.add_module(str(new_idx), g)
-                self.broadcast_modification(self)
-
-                mu = self.calculate_membership(observations)
-
-        if self.expandable:
-            return mu, self.get_mask()
-        else:
-            return mu, None
+        return self.calculate_membership(observations), self.get_mask()
         # return (
         #     torch.exp(
         #         -1.0
