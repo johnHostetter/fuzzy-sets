308ddc140d4d45a01e22bd42151d2c5eb8e31b18
John Wesley Hostetter <jhostetter16@gmail.com>
2024-02-22 11:19:18 -0500

    Refactor to improve Pylint and reduce to Python 3.9

--- soft/fuzzy/sets/continuous/impl.py
+++ soft/fuzzy/sets/continuous/impl.py
@@ -147,7 +147,9 @@ class LogisticCurve(torch.nn.Module):
         Returns:
 
         """
-        return self.supremum / (1 + torch.exp(-self.growth * (tensors - self.midpoint)))
+        return self.supremum / (
+            1 + torch.exp(-1.0 * self.growth * (tensors - self.midpoint))
+        )
 
 
 class Triangular(ContinuousFuzzySet):
