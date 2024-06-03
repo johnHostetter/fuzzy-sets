ccd0426b138a12f502e0727d9fe079cd188a0c56
John Wesley Hostetter <jhostetter16@gmail.com>
2024-06-03 16:24:31 -0400

    Failing 13 unit tests; defuzzification, rule firing, weighing, etc. separated

--- soft/fuzzy/__init__.py
+++ soft/fuzzy/__init__.py
@@ -8,3 +8,15 @@ class TNorm(Enum):
 
     PRODUCT = "product"  # i.e., algebraic product
     MINIMUM = "minimum"
+    ACZEL_ALSINA = "aczel_alsina"  # not yet implemented
+    SOFTMAX_SUM = "softmax_sum"
+    SOFTMAX_MEAN = "softmax_mean"
+    LUKASIEWICZ = "generalized_lukasiewicz"
+    # the following are to be implemented
+    DRASTIC = "drastic"
+    NILPOTENT = "nilpotent"
+    HAMACHER = "hamacher"
+    EINSTEIN = "einstein"
+    YAGER = "yager"
+    DUBOIS = "dubois"
+    DIF = "dif"
